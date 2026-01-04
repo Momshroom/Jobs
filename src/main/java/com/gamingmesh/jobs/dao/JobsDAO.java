1bb08a062fae43336b9d2fee8404598c534aeb7b
Momshroom <Momshroom@gmail.com>
2026-01-04 16:10:41 -0600

    Fix missing +

--- src/main/java/com/gamingmesh/jobs/dao/JobsDAO.java
+++ src/main/java/com/gamingmesh/jobs/dao/JobsDAO.java
@@ -883,7 +883,7 @@ public abstract class JobsDAO {
 
         PreparedStatement prest = null;
         try {
-            prest = conn.prepareStatement("DELETE FROM `" + DBTables.UsersTable.getTableName() + "` WHERE `" + UserTableFields.player_uuid.getCollumn() + "` = `" + uuid "`;`");
+            prest = conn.prepareStatement("DELETE FROM `" + DBTables.UsersTable.getTableName() + "` WHERE `" + UserTableFields.player_uuid.getCollumn() + "` = `" + uuid + "`;`");
             prest.execute();
         } catch (SQLException e) {
             e.printStackTrace();
